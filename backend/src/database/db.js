392c39c0df7308e6f30894a72d2c3702fd315d47
omkharmate <119046339+omkharmate@users.noreply.github.com>
2024-11-21 15:36:04 +0530

    signup route

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -2,7 +2,7 @@ const mongoose =require('mongoose')
 
 async function MongoConnect()
 {
-    try{const a=await mongoose.connect('mongodb+srv://prashantkumardudhmal:Pass%40123@cohortlearning.4tw40.mongodb.net/TravelBuddy');
+    try{const a=await mongoose.connect('mongodb+srv://om:Qj4MsPqXNrHSlQdT@cohortlearning.4tw40.mongodb.net/TravelBuddy');
         console.log("db connected");
     }
     catch(e){
--- backend/src/routes/auth_route.js
+++ backend/src/routes/auth_route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { createUser } = require('../controller/signUpController')
 const  zod = require("zod");
-
+const signUpModel = require("../models/signUpModel");
 
 router.get('/',(req,res)=>{
     res.json({
@@ -11,8 +11,37 @@ router.get('/',(req,res)=>{
 })
 
 
-router.post('/signup',(req,res)=>{
-     createUser;
+router.post('/signup',async (req,res)=>{
+    try{
+       const username=req.body.username;
+       const password=req.body.password;
+       const email=req.body.email;
+
+       let user1 = await signUpModel.findOne({ email: Data.email });
+        if (user1) {
+            return res.status(400).json({
+                success: false,
+                msg: "Sorry, a user with this email already exists!",
+            });
+        }
+
+        let user = await signUpModel.create({
+            name:this.name,
+            email:this.email,
+            password:this.password // Storing plain text password (Not secure)
+        });
+
+        res.status(200).json({
+            success:true,
+            msg:"acc created"
+        })
+    }catch(e){
+        console.log(e);
+        res.json(e);
+    }
+
+
+
 })
 
 
