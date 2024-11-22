181ada4671c5be72bfd5106cf22f5c2164ae864e
omkharmate <119046339+omkharmate@users.noreply.github.com>
2024-11-22 17:27:41 +0530

    signin up/token

--- backend/src/routes/auth_route.js
+++ backend/src/routes/auth_route.js
@@ -3,8 +3,8 @@ const router = express.Router();
 // const { createUser } = require('../controller/signUpController')
 const  zod = require("zod");
 const signUpModel = require("../models/signUpModel");
-const createUser = require("../controller/signUpController");
-
+const {createUser ,signIn}= require("../controller/auth_controller");
+ 
 router.get('/',(req,res)=>{
     res.json({
         msg:"Route works fine"
@@ -13,6 +13,8 @@ router.get('/',(req,res)=>{
 
 
 router.post('/signup',createUser)
+router.post('/signin',signIn)
+
 
 
 
