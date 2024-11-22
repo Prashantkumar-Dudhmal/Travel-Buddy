c8d38655de1f70cd078db01e0724dcaf8461560c
Prashantkumar-Dudhmal <142566433+Prashantkumar-Dudhmal@users.noreply.github.com>
2024-11-22 17:27:36 +0530

    created Profile Controller with get, update , create Profile services

--- backend/src/models/profileModel.js
+++ backend/src/models/profileModel.js
@@ -8,7 +8,7 @@ const UserSchema = new mongoose.Schema({
   rating: { type: Number, default: 5.0 }, // Average rating for user
   totalRatings: { type: Number, default: 0 }, // Total number of ratings received
   isDriver: { type: Boolean, default: false }, // True if the user is offering rides
-  googleId: { type: String, unique: true, sparse: true }, // For Google login users
+  //googleId: { type: String, unique: true, sparse: true }, // For Google login users
   createdAt: { type: Date, default: Date.now },
 });
 
--- backend/src/routes/profile_route.js
+++ backend/src/routes/profile_route.js
@@ -1,54 +1,11 @@
 const express = require("express");
-const ProfileModel = require('../models/profileModel');
-const profileModel = require("../models/profileModel");
+
+const ProfileController = require("../controller/ProfileController");
 const router = express.Router();
 
-router.get("/:id",async(req,res)=>{
-    const userid = req.params.id;
-    let usr = await ProfileModel.findById(userid);
-    console.log(usr);
-    res.json(usr);
-})
-
-router.post("/:id",async(req,res)=>{
-    const userid = req.params.id;
-    const data = req.body;
-    let usr = await ProfileModel.findById(userid);
-    if (!usr) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      // Update user profile
-    usr.name = data.name;
-    usr.email = data.email;
-
-    await usr.save();
-  
-    return res.status(200).json({ message: 'Profile updated successfully'});
-})
-
-router.put("/",async(req,res)=>{
-    const data = req.body;
-    let user = await profileModel.create({
-        name:data.name,
-        email:data.email,
-        phoneNumber:data.phoneNumber,
-        /*name: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
-        phoneNumber: { type: String, required: true, unique: true },
-        profilePhoto: { type: String }, // URL for the uploaded photo
-        rating: { type: Number, default: 5.0 }, // Average rating for user
-        totalRatings: { type: Number, default: 0 }, // Total number of ratings received
-        isDriver: { type: Boolean, default: false }, // True if the user is offering rides
-        googleId: { type: String, unique: true, sparse: true }, // For Google login users
-        createdAt: // Storing plain text password (Not secure)*/
-    });
-    console.log(user);
-    res.json(user);
-})
-
-
-
-
+router.get("/:id",ProfileController.getProfile);
+router.put("/:id",ProfileController.updateProfile);
+router.post("/create",ProfileController.createProfile);
 
 
 module.exports = router;
