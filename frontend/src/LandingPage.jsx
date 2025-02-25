ad5a4bf22c5b5ca1ff7a4860cd49367ea6fcdd90
Prashantkumar-Dudhmal <142566433+Prashantkumar-Dudhmal@users.noreply.github.com>
2025-02-25 16:04:29 +0530

    rough Landing page ready
    Things to look at:
    - Find better font for title
    - Adding animations to make website more alive
    - find a better map image (square) for map tile

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Text } from "@react-three/drei";
 import { CarWhite } from "./components/CarWhite";
 import { degToRad } from "three/src/math/MathUtils.js";
+import { CartoonMap } from "./components/Map";
 
 const LandingPage = () => {
   return (
@@ -9,19 +10,23 @@ const LandingPage = () => {
       <Text
         font={"fonts/Alkatra-VariableFont_wght.ttf"}
         position={[-3, 1, 0]}
-        fontSize={0.8}
+        fontSize={1.3}
+        lineHeight={1}
+        letterSpacing={0.02}
         rotation-y={degToRad(20)}
       >
-        TRAVEL-BUDDY
+        TRAVEL{"\n"}BUDDY
       </Text>
       {
         //Car initial position 15,0,-5 and rotation-y -90
       }
       <CarWhite position={[-2, -1.5, 2]} rotation-y={degToRad(65)} />
-      <mesh position={[2, 0, 1]} rotation-y={degToRad(-20)}>
-        <boxGeometry args={[4, 4, 1]} />
-        <meshLambertMaterial color="hotpink" />
-      </mesh>
+      <CartoonMap
+        position={[1, -1, 1]}
+        rotation-y={degToRad(-105)}
+        scale={0.5}
+      />
+
       <directionalLight position={[5, 10, 5]} intensity={1} />
     </>
   );
